3aff2b750c1657659cdba26a791baa3f777d1e76
Valmontx <valeryevnam.h@gmail.com>
2023-12-13 16:33:44 -0500

    fix-header a Header

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Header } from './header'
+import { Header } from './Header.jsx'
 import { PeopleData } from './PeopleData'
 import { LoadingCell } from './LoadingCell';
 import { ErrorComponent } from './ErrorComponent';
